9b8f480a9fd5e42caa10275216e7efbd8cf0cee9
LukasWillin <lukas.willin@vtxmail.ch>
2020-04-04 20:45:41 +0200

    <1> Setup typescript

--- popup/enlight-yt-popup.ts
+++ popup/enlight-yt-popup.ts
@@ -3,6 +3,8 @@
 //       code: `console.log('location:', window.location.href);`
 //     });
 //   });
+
+
 // // tabs.query doc - https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/Tabs/query
 // return browser.tabs.query({ url: "*://*.youtube.com/*" })
 // .then(tabs =>
@@ -13,6 +15,7 @@
 //     }
 // })
 // .catch(handleError);
+
 // /**
 //          * Insert the page-hiding CSS into the active tab,
 //          * then get the beast URL and
@@ -23,18 +26,21 @@
 //             browser.tabs.insertCSS({code: hidePage}).then(() =>
 //             {
 //                 let url = beastNameToURL(e.target.textContent);
+
 //                 browser.tabs.sendMessage(tabs[0].id, {
 //                     command: "beastify",
 //                     beastURL: url
 //                 });
 //             });
 //         }
+
 //   browser.tabs.removeCSS({code: hidePage}).then(() =>
 //   {
 //       browser.tabs.sendMessage(tabs[0].id, {
 //           command: "reset",
 //       });
 //   });
+
 // /**
 //  * Get the active tab,
 //  * then call "beastify()" or "reset()" as appropriate.
@@ -45,6 +51,7 @@
 //         .then(beastify)
 //         .catch(reportError);
 // }
+
 // /**
 //  * When the popup loads, inject a content script into the active tab,
 //  * and add a click handler.
@@ -53,77 +60,110 @@
 // browser.tabs.executeScript({file: "/content_scripts/beastify.js"})
 //     .then((e) => {  })
 //     .catch(reportExecuteScriptError);
+
 /**
  * Show error in popup and console.
  */
-function handleError(error) {
+function handleError(error)
+{
     document.querySelector("#popup-content").classList.add("hidden");
     document.querySelector("#error-content").classList.remove("hidden");
     document.querySelector("#error-content-exception").innerHTML = JSON.stringify(error);
     console.error(`Error in Enlight-YT: ${error.message}`);
 }
-function sendMessage(tab, message) {
+
+function sendMessage(tab, message) : Promise<object>
+{
     return browser.tabs.sendMessage(tab.id, message)
         .catch(handleError);
 }
-function loadSettings(tab) {
+
+function loadSettings(tab)
+{
     return browser.tabs.sendMessage(tab.id, { 'command': 'get-settings' })
-        .then(response => {
-        console.log("Response from the content script:");
-        console.log(response);
-    }).catch(handleError);
+        .then(response =>
+        {
+            console.log("Response from the content script:");
+            console.log(response);
+        }).catch(handleError);
 }
-function sendMessageToActive(message) {
-    return browser.tabs.query({ active: true, currentWindow: true })
-        .then(tabs => {
-        if (tabs[0])
-            return sendMessage(tabs[0], message);
-        return Promise.resolve(null);
-    });
+
+function sendMessageToActive(message)
+{
+    return browser.tabs.query({active: true, currentWindow: true})
+        .then(tabs => 
+        {
+            if (tabs[0]) 
+                return sendMessage(tabs[0], message);
+
+            return Promise.resolve(null);
+        });
 }
+
 const fieldBrightness = document.querySelector('input#field-brightness');
 const fieldContrast = document.querySelector('input#field-contrast');
 const fieldSaturate = document.querySelector('input#field-saturate');
 const fieldHueRotate = document.querySelector('input#field-hue-rotate');
 const fieldSepia = document.querySelector('input#field-sepia');
-fieldBrightness.addEventListener('change', e => {
+
+fieldBrightness.addEventListener('change', e =>
+{
     console.debug("event-change-brightness:", e);
-    const eventTarget = e.currentTarget;
+
+    const eventTarget : any = e.currentTarget;
+
     sendMessageToActive({
         command: "set-brightness",
         args: [eventTarget.valueAsNumber]
     });
 });
-fieldContrast.addEventListener('change', e => {
+
+fieldContrast.addEventListener('change', e =>
+{
     console.debug("event-change-contrast:", e);
-    const eventTarget = e.currentTarget;
+
+    const eventTarget : any = e.currentTarget;
+
     sendMessageToActive({
         command: "set-contrast",
         args: [eventTarget.valueAsNumber]
     });
 });
-fieldSaturate.addEventListener('change', e => {
+
+fieldSaturate.addEventListener('change', e =>
+{
     console.debug("event-change-saturate:", e);
-    const eventTarget = e.currentTarget;
+
+    const eventTarget : any = e.currentTarget;
+
     sendMessageToActive({
         command: "set-saturate",
         args: [eventTarget.valueAsNumber]
     });
 });
-fieldHueRotate.addEventListener('change', e => {
+
+fieldHueRotate.addEventListener('change', e =>
+{
     console.debug("event-change-hue-rotate:", e);
-    const eventTarget = e.currentTarget;
+
+    const eventTarget : any = e.currentTarget;
+
     sendMessageToActive({
         command: "set-hue-rotate",
         args: [eventTarget.valueAsNumber]
     });
 });
-fieldSepia.addEventListener('change', e => {
+
+fieldSepia.addEventListener('change', e =>
+{
     console.debug("event-change-sepia:", e);
-    const eventTarget = e.currentTarget;
+
+    const eventTarget : any = e.currentTarget;
+
     sendMessageToActive({
         command: "console-log",
         args: [eventTarget.valueAsNumber]
     });
 });
+
 console.log("enlight-yt-popup.js loaded");
